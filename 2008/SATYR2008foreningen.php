26dc0dee7dca8fb65af0c16ae215043192718649
branzoo77 <branzoo@live.dk>
2019-03-23 13:41:07 +0100

    Gamle filer

---
+++ 2008/SATYR2008foreningen.php
@@ -0,0 +1,57 @@
+<?php
+
+$title = "Foreningen (2007)";
+$level = 1;
+
+chdir("..");
+include_once "site/top.php"
+?>
+
+<i>Mel.: The Rembrandts - I'll be there for you</i><br>
+
+<p>Alle studerende vi sidder for os selv<br>
+imens vi hver for sig arbejder os ihjel<br>
+Vi kæmper aldrig under fælles flag<br>
+Men forener vi vor kamp i dag, én sag, så fyr' vi den af!</p>
+
+<p>for...
+Vi kan hjælpes ad (på naturvidenskab)<br>
+Vi kan hjælpes ad (for det er intet tab)<br>
+Vi kan hjælpes ad<br>
+og vi ved du bli'r glad</p>
+
+<p>Med vor forening fejes alle oprør væk<br>
+jura og statskundskab får ikke sagt et kvæk<br>
+Helt underlegne ser de vores front<br>
+Når vi overtager verden hersker vi, som en mastodont</p>
+
+<p>for...
+Vi kan hjælpes ad (på naturvidenskab)<br>
+Vi kan hjælpes ad (for det er intet tab)<br>
+Vi kan hjælpes ad<br>
+og vi ved du bli'r glad</p>
+
+<p>Ingen kan nå vor storhed<br>
+Ingen kan slå satyr ned<br>
+Vi samler alle studierne<br>
+med vor fælles kampsang<br>
+vort fælles flag vil vejre<br>
+Vor fælles kamp vil sejre<br>
+og alle folk vil fejre<br>
+en forening under vor SATYYYYYYYYYYYYR-revy</p>
+
+<p>Helt underlegne ser de vores front<br>
+Når vi overtager verden hersker vi, som en mastodont</p>
+
+<p>for...<br>
+Vi kan hjælpes ad (på naturvidenskab)<br>
+Vi kan hjælpes ad (for det er intet tab)<br>
+Vi kan hjælpes ad<br>
+og vi ved du bli'r glad</p>
+
+<p>Vi kan hjælpes ad<br>
+Vi kan hjælpes ad<br>
+Vi kan hjælpes ad<br>
+og vi tvinger dig glad</p>
+
+<?php include_once "site/bottom.php" ?>
